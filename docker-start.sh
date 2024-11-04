1fc77e45e1d8611c898e5f22d802d5a782c94874
오세은(Se Eun O) <seo@goldenplanet.co.kr>
2024-11-04 10:41:13 +0900

    [build] 도커 실행 스크립트 추기

---
+++ docker-start.sh
@@ -0,0 +1,9 @@
+docker stop kisa-dashboard_proxy-container
+
+docker rm kisa-dashboard_proxy-container
+
+docker rmi $(docker images kisa-dashboard_proxy -q)
+
+docker build -t kisa-dashboard_proxy .
+
+docker docker run --env-file .env -d -p 8080:8080 --name kisa-dashboard_proxy-container kisa-dashboard_proxy
