23de49d6c4618753486ea349d63f04e0196864ee
오세은(Se Eun O) <seo@goldenplanet.co.kr>
2025-03-06 10:32:21 +0900

    [pref] 쿠키 유효 시간 변경

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post('/', async (req, res) => {
 
         const session_id = response.headers.authorization;
         res.cookie('session_id', session_id, {
-            maxAge: 10 * 60 * 1000,
+            maxAge: 3 * 60 * 60 * 1000,
             httpOnly: true,
         });
         res.status(response.status).json(response.data);
